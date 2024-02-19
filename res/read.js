37117bb48d9301f68b3d78eafe156641d543fd60
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-02-19 12:22:33 +0100

    read functionality

--- res/read.js
+++ res/read.js
@@ -1,10 +1,13 @@
 
-
+function makeArray(data){
+        console.log("schisch")
+}
 document.getElementById("inputfile").addEventListener("change", function(){
     let reader = new FileReader()
-
     reader.onload = function(){
+        console.log("reader onload")
         document.getElementById("output").innerText = reader.result
     }
     reader.readAsText(this.files[0])
 })
+
