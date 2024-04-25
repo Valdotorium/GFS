fba9e7408e376859726aa348bf54df07600b9283
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-04-25 11:05:28 +0200

    put graph menu in div

--- res/js/animate.js
+++ res/js/animate.js
@@ -1,8 +1,7 @@
 
 
-
  document.getElementById("duration_slider").oninput = function(){
    //displaying the duration above the slider
     let sliderVal = document.getElementById("duration_slider").value
-    document.getElementById("duration_value").innerText = "Duration:"+sliderVal + "s"
+    document.getElementById("duration_value").innerText = "Duration: "+sliderVal + "s"
  }
