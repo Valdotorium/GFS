fb588ad949df41b66139e2f1c7aed188c767ab69
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-06-19 22:13:04 +0200

    final touches

--- demos/zwischenwerte.py
+++ demos/zwischenwerte.py
@@ -1,5 +1,5 @@
 
-
+#Variablen (hier ohne let erstellt, weil dieses Programm in Python geschrieben ist)
 Anfangswert = 2100
 Endwert = 2500
 FPS = 10
@@ -8,6 +8,7 @@ counter = 0
 
 Wertänderung = Endwert - Anfangswert
 WertänderungProBild = Wertänderung / FPS
+#wiederhole FPS-mal
 while counter < FPS:
     Zwischenwert = Anfangswert + counter * WertänderungProBild
     print(Zwischenwert)
